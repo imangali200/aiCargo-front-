5af49588c56e5c710339eb5a9c955fbf2f17a298
Imangali Zhaksylyk <imangalikazybek1975@gmail.com>
2025-11-14 13:52:22 +0500

    add istory

--- app/pages/superAdmin/track/[productId].vue
+++ app/pages/superAdmin/track/[productId].vue
@@ -4,16 +4,16 @@ import type { Products } from '../tracks.vue'
 const route = useRoute()
 const id = route.params.productId
 
-const product = ref<Products[]>([])
+const product = ref<Products | null>(null)
 
-const {$axios} = useNuxtApp()
+const { $axios } = useNuxtApp()
 
 const token = useCookie('token')
 
-async function getProduct(){
-    const productRes = await $axios.get(`products/${id}`,{
-        headers:{
-            'Authorization':`Bearer ${token.value}`
+async function getProduct() {
+    const productRes = await $axios.get(`products/${id}`, {
+        headers: {
+            'Authorization': `Bearer ${token.value}`
         }
     })
     product.value = productRes.data
@@ -34,7 +34,8 @@ const formatData = (time: string) => {
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
-onMounted(()=>{
+
+onMounted(() => {
     getProduct()
 })
 
@@ -46,16 +47,32 @@ onMounted(()=>{
             <p>⤑</p>
             <router-link class="tw-text-blue-600 hover:tw-underline" to="/superAdmin/tracks"> Товары</router-link>
             <p>⤑</p>
-            <p class="tw-text-gray-400">{{id}}</p>
+            <p class="tw-text-gray-400">{{ id }}</p>
         </div>
-        <div v-if="product">
-            <p>Трек код: <span>{{ id }}</span></p>
-            <p>Создан в: <span>{{ product[0]?.client_registered}}</span></p>
-            <p>{{ product[0]?.china_warehouse }}</p>
-            <div>
-                <p class="tw-bg-green-800 tw-text-white tw-w-[25px] tw-h-[25px] tw-flex tw-items-center tw-justify-center tw-rounded-[50%]">1</p>
-                <p v-if="product[0]?.china_warehouse"></p>
+        <div v-if="product" class="tw-bg-white tw-p-6 tw-rounded-[8px] tw-mt-5">
+            <p class="tw-text-gray-500 tw-border-b-[1px] tw-pb-3">Трек код: <span class="tw-text-black">{{
+                product.productId.toUpperCase() }}</span></p>
+            <p class="tw-text-gray-500 tw-border-b-[1px] tw-py-3">Создан в: <span class="tw-text-black">{{
+                formatData(product.client_registered)}}</span></p>
+            <div class="tw-border-b-[1px] tw-py-3 ">
+                <h3>История:</h3>
+                <div class="tw-flex tw-items-center tw-gap-3 tw-ml-5 tw-mt-2">
+                    <p
+                        class="tw-bg-green-800 tw-text-white tw-w-[25px] tw-h-[25px] tw-flex tw-items-center tw-justify-center tw-rounded-[50%]">
+                        1</p>
+                    <p v-if="product.china_warehouse">{{ formatData(product.china_warehouse)}}</p>
+                    <p v-else>Не выдан</p>
+                </div>
+                <div v-if="product.aicargo" class="tw-flex tw-items-center tw-gap-3 tw-ml-5 tw-mt-2">
+                    <p
+                        class="tw-bg-green-800 tw-text-white tw-w-[25px] tw-h-[25px] tw-flex tw-items-center tw-justify-center tw-rounded-[50%]">
+                        2</p>
+                    <p v-if="product.china_warehouse">{{ formatData(product.aicargo)}}</p>
+                    <p v-else>Не выдан</p>
+                </div>
             </div>
+
+            
 
         </div>
     </div>
